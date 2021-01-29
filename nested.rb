304fb7e87e8c9fdb60781208c97b3be29338fb69
Phong Lo <phonglo724@gmail.com>
2021-01-29 19:37:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+  programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -19,7 +19,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash = 
+  programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+  programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -57,7 +57,126 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-	programmer_hash = 
+  programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  return programmer_hash
+end
+
+def changing_alan
+  programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
+end
+
+def adding_to_dennis
+  programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  return programmer_hash
+end
+
+#or
+
+def hopper
+	programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:grace_hopper]
+end
+
+def alan_kay_is_known_for
+	programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:alan_kay][:known_for]
+end
+
+def dennis_ritchies_language
+	programmer_hash =
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:dennis_ritchie][:languages][0]
+end
+
+def adding_matz
+	programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -78,10 +197,7 @@ def adding_matz
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
-	programmer_hash = 
+	programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -101,10 +217,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
-	programmer_hash = 
+	programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
